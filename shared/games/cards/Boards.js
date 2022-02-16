74339cbc141fd2aea647aaf71e1111269da78be4
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-02-16 17:45:58 +0000

    worked on slots scaffolding and positional piles

--- shared/games/cards/Boards.js
+++ shared/games/cards/Boards.js
@@ -3,11 +3,25 @@ const CARD_FACING = {
     UP: 'up',
     DOWN: 'down'
 };
+
+const PILES = {
+    COMMON_DISCARD: 'common_discard_pile',
+    DISCARD: 'discard_pile',
+    PLAYERS: 'players_piles',
+    PLAYER: 'player_piles',
+};
 class PositionalBoard extends Board {
-    constructor() {
+    constructor(players = []) {
         super();
         this.board = new Map();
         this.cards = [];
+        this.piles = {
+            [PILES.COMMON_DISCARD]: [],
+            [PILES.PLAYERS]: players.map(({id})=> ({
+                [PILES.PLAYER]:[], 
+                [PILES.DISCARD]: []
+            })),
+        };
     }
 
     place(card, playerId, position = [0, 0], facing = CARD_FACING.UP) {
@@ -71,21 +85,49 @@ class PositionalBoard extends Board {
 
 
 const defaultSlotsConfiguration = {
-    slots: {}
+    slots: () => new Slot()
 };
+
+class Slot {
+    constructor(name, ownerId = null) {
+        this.name = name;
+        this.ownerId = ownerId;
+        this.stack = [];
+        this.cards = [];
+    }
+
+    place(card, facing) {
+        this.cards.push(card.id);
+        this.stack.push({ card, facing });
+    }
+
+    remove(howMany = 1) {
+        const removed = [];
+        for (let i = 0; i < howMany; i++) {
+            removed.push(this.stack.pop());
+        }
+
+        const removedIds = removed.map(({ id }) => id);
+        this.cards = this.cards.filter(id => !removedIds.includes(id));
+
+        return removedIds;
+    }
+}
+
+// maybe wont need this ↑ and I could collapse it into this ↓
 class SlotsBoard extends PositionalBoard {
     constructor(config = {}) {
         super();
         config = { ...defaultSlotsConfiguration, ...config };
-        this.slots = config.slots;
+        this.slots = config.slots();
         this.cards = [];
     }
 
-    place(card, playerId, position, facing = CARD_FACING.UP) {
+    place(card, playerId, slot, facing = CARD_FACING.UP) {
         // need to handle duplicates if double decks too
         // if this.has card then change id adding player or maybe a random id
         // TODO
-        this.board.set(card.id, { card, playerId, facing, position });
+        this.slots.place(slot, { card, playerId, facing, slot });
         this.cards.push(card.id);
     }
 
